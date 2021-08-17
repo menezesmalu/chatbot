77a4fc48f2e89febb4eb29c1d4f406251033906a
Eden Ernandes <eden.santana@neurotech.com.br>
2021-08-17 02:02:07 -0300

    Add actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,42 @@
 # This files contains your custom actions which can be used to run
 # custom Python code.
-#
+
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from elasticsearch import Elasticsearch
+
+class ActionGetFunFact(Action):
+    def name(self) -> Text:
+        return "action_get_fun_fact"
+
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        es = Elasticsearch(HOST="http://localhost", PORT=9200)
+
+        question = tracker.latest_message['text']
+        body = {
+            "from":0,
+            "size":100,
+            "query": {
+                "match": {
+                    "question":question
+                }
+            }
+        }
+        response = es.search(index="movies_index", body=body)
+        final_answer = response['hits']['hits'][0]['_source']['answer']
+        
+
+        dispatcher.utter_message(text=final_answer)
+
+        return []
